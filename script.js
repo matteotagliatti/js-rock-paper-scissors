8b61821af8062463bc8c0f6c13419739cfd714d9
matteotagliatti <teotagliatti@gmail.com>
2021-10-28 17:00:44 +0200

    modal, overlay, placeholder.png

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let playerWon = 0;
 // GAME LOGIC
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = getRandomChoice()
-
     if (playerSelection === computerSelection) {
         roundWinner = 'tie'
     }
@@ -44,28 +42,36 @@ function getRandomChoice() {
 rockBtn.addEventListener('click', () => playerChoice('rock'))
 paperBtn.addEventListener('click', () => playerChoice('paper'))
 scissorsBtn.addEventListener('click', () => playerChoice('scissors'))
+restart.addEventListener('click', restartGame)
 
 const resultInfo = document.getElementById('resultInfo')
 const playerScore = document.getElementById('playerScore')
 const computerScore = document.getElementById('computerScore')
 const finalResult = document.getElementById('finalResult')
+const overlay = document.getElementById('overlay')
+const modal = document.getElementById('modal')
 
 function playerChoice(playerSelection) {
     const computerSelection = getRandomChoice()
     playRound(playerSelection, computerSelection)
+    updateChoices(playerSelection, computerSelection)
     updateScore()
     countRounds()
 }
 
 function updateScore() {
     if (roundWinner === 'tie') {
-        resultInfo.textContent = "It's a tie!"
+        resultInfo.innerHTML = "Round winner: <strong>Tie!</strong>"
     } else if (roundWinner === 'player') {
-        resultInfo.textContent = "You won!"
+        resultInfo.innerHTML = "Round winner: <strong>You!</strong>"
     } else if (roundWinner === 'computer') {
-        resultInfo.textContent = "You lost!"
+        resultInfo.innerHTML = "Round winner: <strong>Computer!</strong>"
     }
 
+    updatePoints()
+}
+
+function updatePoints() {
     playerScore.textContent = `Player: ${playerWon}`
     computerScore.textContent = `Computer: ${computerWon}`
 }
@@ -78,11 +84,29 @@ function countRounds() {
             finalResult.textContent = "You lost 🙁"
         }
 
-        restartGame()
+        showEndModal()
     }
+}
+
+function showEndModal() {
+    overlay.classList.remove('hid')
+    modal.classList.remove('hid')
+}
+
+function updateChoices(playerSelection, computerSelection) {
+    const playerChoice = document.getElementById('playerChoice')
+    const computerChoice = document.getElementById('computerChoice')
+
+    playerChoice.innerHTML = `<img src="img/${playerSelection}.png">`
+    computerChoice.innerHTML = `<img src="img/${computerSelection}.png">`
 }
 
 function restartGame() {
     playerWon = 0
     computerWon = 0
+    updatePoints()
+    updateChoices('placeholder', 'placeholder')
+    resultInfo.innerHTML = "Round winner:"
+    overlay.classList.add('hid')
+    modal.classList.add('hid')
 }
